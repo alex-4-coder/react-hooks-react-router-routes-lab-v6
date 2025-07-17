e2b2f98f00030a2e6de39c5ab453a2b27fb7aa2c
alex kamau <alex.kamau1@student.moringaschool.com>
2025-07-17 13:25:21 -0700

    complete lab

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "./NavBar.css";
 
 function NavBar() {
   return (
-    <nav>
+    <div className="navbar">
       <NavLink to="/">Home</NavLink>
-      <NavLink to="/directors">Directors</NavLink>
       <NavLink to="/actors">Actors</NavLink>
-    </nav>
+      <NavLink to="/directors">Directors</NavLink>
+    </div>
   );
 }
 
--- src/pages/Actors.jsx
+++ src/pages/Actors.jsx
@@ -11,22 +11,21 @@ function Actors() {
   }, []);
 
   return (
-    <>
+    <div>
       <NavBar />
       <h1>Actors Page</h1>
-      {actors.map((actor) => (
-        <article key={actor.name}>
+      {actors.map((actor, idx) => (
+        <article key={idx}>
           <h2>{actor.name}</h2>
           <ul>
-            {actor.movies.map((movie, index) => (
-              <li key={index}>{movie}</li>
+            {actor.movies.map((movie, i) => (
+              <li key={i}>{movie}</li>
             ))}
           </ul>
         </article>
       ))}
-    </>
+    </div>
   );
 }
 
 export default Actors;
-
